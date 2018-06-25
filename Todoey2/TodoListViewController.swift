9b6cd00b1d03d74eef65838e756c9829cfea74ca
Ashwani Agrawal <steveash98@gmail.com>
2018-06-25 18:26:05 +0530

    add item added with data persistence using user defaults

--- Todoey2/TodoListViewController.swift
+++ Todoey2/TodoListViewController.swift
@@ -11,13 +11,44 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var listItem = ["one","two","three"]
+    var defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            listItem = items
+        }
+        
         tableView.separatorStyle = .none
         
     }
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Item", message: "enter the item", preferredStyle: .alert)
+        let action =  UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            self.listItem.append(textField.text!)
+            self.defaults.set(self.listItem, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
+          //  print("333")
+        }
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "enter item"
+            textField = alertTextField
+            //print("1" + textField.text!)
+        }
+        //print("2" + textField.text!)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listItem.count
     }
