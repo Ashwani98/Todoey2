f7677975de1682056f3b6622d65a8e8336885221
Ashwani Agrawal <steveash98@gmail.com>
2018-06-26 15:28:27 +0530

    "data persistence using NSCoder"

---
+++ Todoey2/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey2
+//
+//  Created by Ashwani  Agrawal on 26/06/18.
+//  Copyright © 2018 Ashwani  Agrawal. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var isChecked = false
+    var value = ""
+}
--- Todoey2/TodoListViewController.swift
+++ Todoey2/TodoListViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var listItem = ["one","two","three"]
-    var defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    var path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(path!)
+        getData()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            listItem = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+//            itemArray = items
+//        }
+//        var item1 = Item()
+//        item1.value = "sddf"
+//        var item2 = Item()
+//        item2.value = "sknjfn"
+//        var item3 = Item()
+//        item3.value = "iiiii"
+//        itemArray.append(item1)
+//        itemArray.append(item2)
+//        itemArray.append(item3)
+//
+        
         
         tableView.separatorStyle = .none
         
@@ -29,9 +44,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new Item", message: "enter the item", preferredStyle: .alert)
         let action =  UIAlertAction(title: "Add", style: .default) { (action) in
             
-            self.listItem.append(textField.text!)
-            self.defaults.set(self.listItem, forKey: "TodoListArray")
-            
+            let item = Item()
+            item.value = textField.text!
+            self.itemArray.append(item)
+            self.saveData()
             self.tableView.reloadData()
           //  print("333")
         }
@@ -45,27 +61,56 @@ class TodoListViewController: UITableViewController {
         //print("2" + textField.text!)
         
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
+    }
+    
+
+    func saveData(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: path!)
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    func getData(){
+        if let data = try? Data(contentsOf: path!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch{
+                print(error)
+            }
+        }
+        
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listItem.count
+        return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = listItem[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].value
+        
+        cell.accessoryType = itemArray[indexPath.row].isChecked ? .checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(listItem[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        print(itemArray[indexPath.row].value)
+        
+        itemArray[indexPath.row].isChecked = !itemArray[indexPath.row].isChecked
+        saveData()
+        tableView.reloadData()
+
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
